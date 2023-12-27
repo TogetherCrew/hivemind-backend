57b4761e479d0cc21d11b12d5b7ddc88ca121d62
Mohammad Amin Dadgar <48308230+amindadgar@users.noreply.github.com>
2023-12-27 09:22:14 +0330

    Initial commit

---
+++ tc_python_library_template/tests/unit/test_return_results.py
@@ -0,0 +1,21 @@
+from tc_python_library_template import Sample
+
+
+def test_random_computations_have_results():
+    """
+    test if the random copmutations results is not None
+    """
+    sample = Sample()
+    result = sample.sample_compute()
+
+    assert result is not None
+
+
+def test_random_computations_integer_results():
+    """
+    test if the random computation result is integer
+    """
+    sample = Sample()
+    result = sample.sample_compute()
+
+    assert type(result) is int
