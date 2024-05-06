a2dc150b8f15971caaf86b937903ce1b75c07a8c
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-06 09:55:14 +0330

    feat: updating the llama-index-core lib version!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="tc-hivemind-backend",
-    version="1.1.5",
+    version="1.1.6",
     author="Mohammad Amin Dadgar, TogetherCrew",
     maintainer="Mohammad Amin Dadgar",
     maintainer_email="dadgaramin96@gmail.com",
