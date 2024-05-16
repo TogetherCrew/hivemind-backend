85ad95bb4561bdd07716d405e3aa85d73212347b
Mohammad Amin <dadgaramin96@gmail.com>
2024-05-16 13:08:51 +0330

    feat: Adding qdrant vector db support!

--- tc_hivemind_backend/db/credentials.py
+++ tc_hivemind_backend/db/credentials.py
@@ -25,3 +25,39 @@ def load_postgres_credentials() -> dict[str, str]:
     credentials["db_name"] = os.getenv("POSTGRES_DBNAME", "")
 
     return credentials
+
+
+def load_qdrant_credentials() -> dict[str, str]:
+    """
+    load qdrant database credentials
+
+    Returns:
+    ---------
+    qdrant_creds : dict[str, Any]
+        redis credentials
+        a dictionary representive of
+            `api_key` : str
+            `host` : str
+            `port` : int
+    """
+    load_dotenv()
+
+    qdrant_creds: dict[str, str] = {}
+
+    host = os.getenv("QDRANT_HOST")
+    port = os.getenv("QDRANT_PORT")
+    api_key = os.getenv("QDRANT_API_KEY")
+
+    if host is None:
+        raise ValueError("`QDRANT_HOST` is not set in env credentials!")
+    if port is None:
+        raise ValueError("`QDRANT_PORT` is not set in env credentials!")
+    if api_key is None:
+        raise ValueError("`QDRANT_API_KEY` is not set in env credentials!")
+
+    qdrant_creds = {
+        "host": host,
+        "port": port,
+        "api_key": api_key,
+    }
+    return qdrant_creds
